16ab5ed4e3e47f601a68cee3e093d7fcee5a5ea8
avidrucker <avidrucker@gmail.com>
2023-12-01 11:41:37 -0500

    fix check for env vars in chatbot script

--- chatbot.js
+++ chatbot.js
@@ -7,6 +7,12 @@ const octokit = new Octokit({
 async function postGreeting() {
   const issueNumber = process.env.ISSUE_NUMBER;
   const repoName = process.env.GITHUB_REPOSITORY;
+
+  if (!issueNumber || !repoName) {
+    console.error('Required environment variables are missing');
+    process.exit(1);
+  }
+
   const [owner, repo] = repoName.split('/');
 
   const message = `Hello! Thanks for creating an issue. Please follow our [contribution guidelines](LINK_TO_YOUR_GUIDELINES). If you have any questions, feel free to create new issues.`;
