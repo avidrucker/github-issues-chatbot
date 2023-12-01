11da3006fe470580455bf2b9b2f969309ecc80e5
avidrucker <avidrucker@gmail.com>
2023-12-01 12:09:45 -0500

    feat greet issue creator by username

--- chatbot.js
+++ chatbot.js
@@ -7,15 +7,16 @@ const octokit = new Octokit({
 async function postGreeting() {
   const issueNumber = process.env.ISSUE_NUMBER;
   const repoName = process.env.GITHUB_REPOSITORY;
+  const issueCreator = process.env.ISSUE_CREATOR;
 
-  if (!issueNumber || !repoName) {
+  if (!issueNumber || !repoName || !issueCreator) {
     console.error('Required environment variables are missing');
     process.exit(1);
   }
 
   const [owner, repo] = repoName.split('/');
 
-  const message = `Hello! Thanks for creating an issue. Please follow our [contribution guidelines](LINK_TO_YOUR_GUIDELINES). If you have any questions, feel free to create new issues.`;
+  const message = `Hello @${issueCreator}! Thanks for creating an issue. Please follow our [contribution guidelines](LINK_TO_YOUR_GUIDELINES). If you have any questions, feel free to create new issues.`;
 
   await octokit.issues.createComment({
     owner,
