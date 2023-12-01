2bce09bb32cea6948b512b21993b7041c1306a03
avidrucker <avidrucker@gmail.com>
2023-12-01 11:18:28 -0500

    first draft chatbot

---
+++ chatbot.js
@@ -0,0 +1,23 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+});
+
+async function postGreeting() {
+  const issueNumber = process.env.ISSUE_NUMBER;
+  const repoName = process.env.GITHUB_REPOSITORY;
+  const [owner, repo] = repoName.split('/');
+
+  const message = `Hello! Thanks for creating an issue. Please follow our [contribution guidelines](LINK_TO_YOUR_GUIDELINES). If you have any questions, feel free to create new issues.`;
+
+  await octokit.issues.createComment({
+    owner,
+    repo,
+    issue_number: issueNumber,
+    body: message
+  });
+}
+
+postGreeting().catch(err => console.error(err));
+
